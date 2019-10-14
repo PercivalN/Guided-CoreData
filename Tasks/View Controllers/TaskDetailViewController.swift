c0106f0c11a7761212a8ceb6e8aeaab344b3d507
Percy <percival.ngan@gmail.com>
2019-10-14 13:20:21 -0500

    Added TaskController

---
+++ Tasks/View Controllers/TaskDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by Percy Ngan on 10/14/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+	var taskController: TaskController?
+	var task: Task?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
